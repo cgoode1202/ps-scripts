767890b8c8073ed8dd8e814fd4b8e540554f2332
cgoode1202 <cgoode1202@gmail.com>
2021-12-02 21:01:11 -0800

    first commit

---
+++ MyScript.ps1
@@ -0,0 +1,15 @@
+﻿# Import the Azure PowerShell module.
+Import-Module -Name Az
+
+# Connect to an Azure account.
+Connect-AzAccount
+
+# Define Azure variables for a virtual machine.
+$vmName = "WingtiptoysVM"
+$resourceGroup = "WingtiptoysRG"
+
+# Create Azure credentials.
+$adminCredential = Get-Credential -Message "Enter a username and password for the VM administrator."
+
+# Create a virtual machine in Azure.
+New-AzVm -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredential -Image UbuntuLTS
