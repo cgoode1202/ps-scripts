a2b1f3956b2856c3bc1d26a805ee5b7f175aa3ab
cgoode1202 <cgoode1202@gmail.com>
2021-12-12 10:01:37 -0800

    Completed adding VM creation process and successful connection

--- SecureVNetDeployment.ps1
+++ SecureVNetDeployment.ps1
@@ -28,3 +28,29 @@ $Subnet=New-AzVirtualNetworkSubnetConfig -Name default -AddressPrefix 10.0.0.0/2
 
  # Get the public IP address of the newly created VM
  Get-AzPublicIpAddress -Name dataProcStage1
+
+ # Create second VM
+ New-AzVm `
+ -ResourceGroupName "vm-networks" `
+ -Name "dataProcStage2" `
+ -VirtualNetworkName "myVnet" `
+ -SubnetName "default" `
+ -image "Win2016Datacenter" `
+ -Size "Standard_DS2_v2"
+
+ # Disassociate the public IP address tath was created by default for the VM
+$nic = Get-AzNetworkInterface -Name dataProcStage2 -ResourceGroup vm-networks
+$nic.IpConfigurations.publicipaddress.id = $null
+Set-AzNetworkInterface -NetworkInterface $nic
+
+# Connect to VM from PS command line
+mstsc /v:publicIpAddress
+
+# Launch 2nd VM from within the 1st
+# On dataProcStage2, select the Start Menu, enter Firewall, and press Enter
+# In the right-hand pane, scroll down, right-click File and Printer Sharing 
+# (Echo Request - ICMPv4-In), and then confirm that Enable Rule is enabled.
+# Switch back to the dataProcStage1 remote session, and run the following command in the command prompt
+ping dataProcStage2 -4
+
+# You can now ping the 2nd VM from the 1st one
