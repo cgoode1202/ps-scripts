90238e0541077a1edb3204d96217dfe5f8853a4f
cgoode1202 <cgoode1202@gmail.com>
2021-12-11 17:19:53 -0800

    added key vault steps

--- SecureVMDeployment.ps1
+++ SecureVMDeployment.ps1
@@ -12,15 +12,40 @@ New-AzVm `
     -Location $location `
     -OpenPorts 3389
 
-<#After the VM finishes deploying, capture the VM details in a variable. 
+<# After the VM finishes deploying, capture the VM details in a variable. 
 You can use this variable to explore what was created.#>
 $vm = Get-AzVM -Name $vmName -ResourceGroupName $rgName
 
 You can see the OS disk attached to the VM.
 $vm.StorageProfile.OSDisk
 
-<#Check the current status of encryption on the OS disk (and any data disks).#>
+<# Check the current status of encryption on the OS disk (and any data disks).#>
 Get-AzVmDiskEncryptionStatus  `
     -ResourceGroupName $rgName `
     -VMName $vmName
 
+<# Designate a name for your Key Vault#>
+$keyVaultName = "mvmdsk-kv-1234"
+
+<# Create an Azure Key Vault#>
+New-AzKeyVault -VaultName $keyVaultName `
+    -Location $location `
+    -ResourceGroupName $rgName `
+    -EnabledForDiskEncryption
+
+<# Store Key Vault information#>
+$keyVault = Get-AzKeyVault `
+    -VaultName $keyVaultName `
+    -ResourceGroupName $rgName
+
+<# Encrypt the VM disks#>
+Set-AzVmDiskEncryptionExtension `
+	-ResourceGroupName $rgName `
+    -VMName $vmName `
+    -VolumeType All `
+	-DiskEncryptionKeyVaultId $keyVault.ResourceId `
+	-DiskEncryptionKeyVaultUrl $keyVault.VaultUri `
+    -SkipVmBackup
+
+<# After its completion, check the encryption status again#>
+Get-AzVmDiskEncryptionStatus  -ResourceGroupName $rgName -VMName $vmName
