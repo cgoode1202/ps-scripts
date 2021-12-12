036b6a9ef1362c6114088f55453f6d61b59cc274
cgoode1202 <cgoode1202@gmail.com>
2021-12-12 09:03:06 -0800

    tidying up comments

--- SecureVMDeployment.ps1
+++ SecureVMDeployment.ps1
@@ -1,44 +1,44 @@
-<# To hold the selected location, define a PowerShell variable#>
+# To hold the selected location, define a PowerShell variable
 $location = (Get-AzResourceGroup -name testVM-rg).location
 
-<# Next, define a few more convenient variables to capture the name of the VM and the resource group#>
+# Next, define a few more convenient variables to capture the name of the VM and the resource group
 $vmName = "fmdata-vm01"
 $rgName = "testVM-rg"
 
-<# Create a new VM#>
+# Create a new VM
 New-AzVm `
     -ResourceGroupName $rgName `
     -Name $vmName `
     -Location $location `
     -OpenPorts 3389
 
-<# After the VM finishes deploying, capture the VM details in a variable. 
-You can use this variable to explore what was created.#>
+# After the VM finishes deploying, capture the VM details in a variable. 
+# You can use this variable to explore what was created.
 $vm = Get-AzVM -Name $vmName -ResourceGroupName $rgName
 
 You can see the OS disk attached to the VM.
 $vm.StorageProfile.OSDisk
 
-<# Check the current status of encryption on the OS disk (and any data disks).#>
+# Check the current status of encryption on the OS disk (and any data disks).
 Get-AzVmDiskEncryptionStatus  `
     -ResourceGroupName $rgName `
     -VMName $vmName
 
-<# Designate a name for your Key Vault#>
+# Designate a name for your Key Vault
 $keyVaultName = "mvmdsk-kv-1234"
 
-<# Create an Azure Key Vault#>
+# Create an Azure Key Vault
 New-AzKeyVault -VaultName $keyVaultName `
     -Location $location `
     -ResourceGroupName $rgName `
     -EnabledForDiskEncryption
 
-<# Store Key Vault information#>
+# Store Key Vault information
 $keyVault = Get-AzKeyVault `
     -VaultName $keyVaultName `
     -ResourceGroupName $rgName
 
-<# Encrypt the VM disks#>
+# Encrypt the VM disks
 Set-AzVmDiskEncryptionExtension `
 	-ResourceGroupName $rgName `
     -VMName $vmName `
@@ -47,5 +47,5 @@ Set-AzVmDiskEncryptionExtension `
 	-DiskEncryptionKeyVaultUrl $keyVault.VaultUri `
     -SkipVmBackup
 
-<# After its completion, check the encryption status again#>
+# After its completion, check the encryption status again
 Get-AzVmDiskEncryptionStatus  -ResourceGroupName $rgName -VMName $vmName
