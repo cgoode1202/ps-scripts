4becf19518c03af784c79f80af4912cf943a7df9
cgoode1202 <cgoode1202@gmail.com>
2021-12-15 12:10:44 -0800

    updating Azure AD file

--- AddAzureADUser.ps1
+++ AddAzureADUser.ps1
@@ -6,3 +6,9 @@ $PasswordProfile.Password = "<Password>"
 
 # Create the new user
 New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -PasswordProfile $PasswordProfile -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contoso.com"
+
+# Create a new group
+New-AzureADGroup -Description "Marketing" -DisplayName "Marketing" -MailEnabled $false -SecurityEnabled $true -MailNickName "Marketing"
+
+# Get role information
+Get-AzRoleDefinition Owner
