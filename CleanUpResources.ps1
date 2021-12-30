d64d4ae3132e79996b1aec68bdef05441652b2ea
cgoode1202 <cgoode1202@gmail.com>
2021-12-30 11:52:37 -0800

    adding CleanUpResources file

---
+++ CleanUpResources.ps1
@@ -0,0 +1,9 @@
+# List all resource groups created
+Get-AzResourceGroup -Name '[RESOURCE GROUP NAME]*'
+
+# Delete all resource groups
+Get-AzResourceGroup -Name '[RESOURCE GROUP NAME]*' | Remove-AzResourceGroup -Force -AsJob
+
+# The command executes asynchronously (as determined by the -AsJob parameter), so while you will 
+# be able to run another PowerShell command immediately afterwards within the same PowerShell session, 
+# it will take a few minutes before the resource groups are actually removed.
