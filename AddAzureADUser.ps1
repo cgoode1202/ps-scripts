3004085e259739067a9c5da8ed7c1bb3a23d67aa
cgoode1202 <cgoode1202@gmail.com>
2023-04-28 18:04:26 -0700

    added Azure AD and storage Powershell commands

--- AddAzureADUser.ps1
+++ AddAzureADUser.ps1
@@ -1,14 +1,37 @@
+# Connect to Azure AD
+Connect-AzureAD
+
 # Create a password object
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 
 # Assign the password
 $PasswordProfile.Password = "<Password>"
 
+# Identify the name of your Azure AD tenant
+$domainName = ((Get-AzureAdTenantDetail).VerifiedDomains)[0].Name
+
 # Create the new user
-New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -PasswordProfile $PasswordProfile -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contoso.com"
+New-AzureADUser -DisplayName 'Isabel Garcia' -PasswordProfile "$passwordProfile" -UserPrincipalName "Isabel@$domainName" -AccountEnabled $true -MailNickName 'Isabel'
 
 # Create a new group
 New-AzureADGroup -Description "Marketing" -DisplayName "Marketing" -MailEnabled $false -SecurityEnabled $true -MailNickName "Marketing"
 
 # Get role information
 Get-AzRoleDefinition Owner
+
+# Create a new security group
+New-AzureADGroup -DisplayName 'Junior Admins' -MailEnabled $false -SecurityEnabled $true -MailNickName JuniorAdmins
+
+# List groups in your AAD tenant
+Get-AzureADGroup
+
+# Add a member to a group
+$user = Get-AzureADUser -Filter "MailNickName eq 'Isabel'"
+Add-AzADGroupMember -MemberUserPrincipalName $user.userPrincipalName -TargetGroupDisplayName "Junior Admins"
+Add-AzureADGroupMember -ObjectId "<Group Object Id" -RefObjectId "User Object Id"
+
+# Add an owner to a group
+Add-AzureADGroupOwner -ObjectId "<Group Object Id" -RefObjectId "User Object Id"
+
+# List details of a role
+Get-AzRoleDefinition -Name '<roleName>'
--- MyScript.ps1
+++ MyScript.ps1
@@ -4,6 +4,12 @@ Import-Module -Name Az
 # Connect to an Azure account.
 Connect-AzAccount
 
+# Create a resource group
+New-AzResourceGroup -Location 'location' -Name 'name'
+
+# Retrivew list of resource groups
+Get-AzResourceGroup
+
 # Define Azure variables for a virtual machine.
 $vmName = "WingtiptoysVM"
 $resourceGroup = "WingtiptoysRG"
@@ -22,3 +28,19 @@ Set-Content test.txt 'here is some sample text'
 
 # Read content of file
 get-Content test.txt
+
+# Deploy a bicep template from Azure CLI
+az deployment group create --template-file main.bicep --resource-group $rgName
+
+# Create a new storage account with a randomized name
+New-AzStorageAccount -ResourceGroupName AZ500LAB03 -Name (Get-Random -Maximum 999999999999999) -Location  EastUS -SkuName Standard_LRS -Kind StorageV2 
+
+# Remove delete lock
+$storageaccountname = (Get-AzStorageAccount -ResourceGroupName AZ500LAB03).StorageAccountName
+
+$lockName = (Get-AzResourceLock -ResourceGroupName AZ500LAB03 -ResourceName $storageAccountName -ResourceType Microsoft.Storage/storageAccounts).Name
+
+Remove-AzResourceLock -LockName $lockName -ResourceName $storageAccountName  -ResourceGroupName AZ500LAB03 -ResourceType Microsoft.Storage/storageAccounts -Force
+
+# Check if you are able to create VMs in a given region
+az vm list-skus --location 'region' -o table --query "[? contains(name,'Standard_D2s')].name"
