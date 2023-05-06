5e04f25ad97c7b1afbedbeb42b57a2eea6a93331
cgoode1202 <cgoode1202@gmail.com>
2023-05-05 17:24:40 -0700

    added static website powershell commands

---
+++ StaticWebsite.ps1
@@ -0,0 +1,28 @@
+# Define variables
+$resourceGroupName = "static-website-rg"
+$location = "westus"
+$storageName = "staticwebsitedemo678972"
+$storageSku = "Standard_GRS"
+
+# Create a resource group
+New-AzResourceGroup -Name $ResourceGroupName -location $location
+
+# Create a storage account
+$storageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupname -Name $storageName -Location $location -SkuName $storagesku
+
+# Set the current default storage account
+Set-AzCurrentStorageAccount -ResourceGroupName $ResourceGroupName -Name $storageName
+
+# Get context of the storage account for next commands
+$ctx = $storageAccount.Context
+
+# Enable static website
+Enable-AzStorageStaticWebsite -IndexDocument "index.html" -ErrorDocument404Path "index.html"
+
+# Upload a file making sure to set the content type to text/html
+set-AzStorageblobcontent -File "index.html" -Container `$web -Blob "index.html" -Context $ctx -Properties @{ ContentType = "text/html; charset=utf-8";}
+
+# Now you have a static website up and running on Azure!
+# Use these commands to get the public URL
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageName
+Write-Output $storageAccount.PrimaryEndpoints.Web
